c4bcaa31b5eeebf0e11c907e3a40c7dfa43e2e4e
Sergei Schneidmiller <5797221@gmail.com>
2023-03-14 16:39:14 +0300

    dz1 ready

---
+++ dz1/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Введите трехзначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+int num1 = (num / 10) % 10;
+Console.WriteLine("Вторая цифра введенного числа " + num1);
