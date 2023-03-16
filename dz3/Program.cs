a080a8c92716b506bfc5bd45ff0de2a1f2fe1c10
Sergei Schneidmiller <5797221@gmail.com>
2023-03-16 19:39:54 +0300

    dz3 ready

---
+++ dz3/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+
+void CheckDayIndex (int index)
+{
+    if (index > 0 & index < 6)
+    {
+        Console.WriteLine ("Этот день - будний");
+    }
+    else if (index > 5 & index < 8)
+    {
+        Console.WriteLine ("Этот день - выходной");
+    }
+    else
+    {
+        Console.WriteLine ("Введите число от 1 до 7!");
+    }
+}
+
+
+
+try
+{
+    Console.WriteLine("Введите порядковый номер дня недели");
+    int index = Convert.ToInt32(Console.ReadLine());
+    CheckDayIndex (index);
+}
+catch
+{
+    Console.WriteLine("Введено некорректное значение");
+}
