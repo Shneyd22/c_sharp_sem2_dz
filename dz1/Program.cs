56d93526122b35a104b72084e8abfb72a0e89700
Sergei Schneidmiller <5797221@gmail.com>
2023-03-16 20:19:02 +0300

    dz2 add function

--- dz1/Program.cs
+++ dz1/Program.cs
@@ -1,4 +1,25 @@
-﻿Console.WriteLine("Введите трехзначное число");
+﻿void SecondNum (int num)
+{
+    if (num > -1000 & num < 1000)
+    {
+    num = (num / 10) % 10;
+    Console.WriteLine("Вторая цифра введенного числа " + num);
+    }
+    else
+    {
+       Console.WriteLine("Введите трехзначное число!!!"); 
+    }
+}
+
+
+
+try
+{
+Console.WriteLine("Введите трехзначное число");
 int num = Convert.ToInt32(Console.ReadLine());
-int num1 = (num / 10) % 10;
-Console.WriteLine("Вторая цифра введенного числа " + num1);
+SecondNum (num);
+}
+catch
+{
+   Console.WriteLine("Введено некорректное значение"); 
+}
