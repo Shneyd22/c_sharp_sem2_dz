1c33ab9f931d3fa6258e84b8212cd3669a4121f8
Sergei Schneidmiller <5797221@gmail.com>
2023-03-16 18:26:44 +0300

    dz2 ready

---
+++ dz_2/Program.cs
@@ -0,0 +1,38 @@
+﻿int ThirdRank(int number)
+{
+    while (number > 999)
+    {
+        number = number / 10;
+    }
+    return number % 10;
+}
+
+
+
+
+bool CheckNum(int number)
+{
+    if (number < 100)
+    {
+        Console.WriteLine ("Третья цифра отсутствует");
+        return false;
+    }
+    return true;
+}
+
+
+
+
+try
+{
+    Console.WriteLine("Введите число");
+    int num = Convert.ToInt32(Console.ReadLine());   
+    if (CheckNum(num))
+    {
+        Console.WriteLine(ThirdRank(num));
+    }
+}
+catch
+{
+Console.WriteLine("Введено неверное значение");
+}
